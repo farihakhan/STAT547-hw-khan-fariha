f2972c4d42000ae4e8a73da141fd5f47878f58ec
farihakhan <fariha83@hotmail.com>
2017-12-08 16:11:19 -0800

    Adding files

---
+++ hw_10/rScripts/01_scrapeData.r
@@ -0,0 +1,116 @@
+suppressPackageStartupMessages(library(tidyverse)) 
+suppressPackageStartupMessages(library(knitr))
+suppressPackageStartupMessages(library(kableExtra))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(rvest))
+
+
+# Get URL info ---------------------------------------------------------------------------
+
+imdb_home <- "http://www.imdb.com"
+imdb_url <- "http://www.imdb.com/chart/top?ref_=nv_mv_250_6"
+imdb <- read_html(imdb_url)
+
+
+# Extract fields -------------------------------------------------------------------------
+
+extractFields <- function(x){
+      
+      Title <- imdb %>% 
+            html_nodes(".titleColumn") %>% 
+            html_nodes("a, #text") %>% 
+            html_text()
+      
+      yearRelease <- imdb %>% 
+            html_nodes(".secondaryInfo") %>% 
+            html_text() %>% 
+            str_replace_all(c("[(|)]" = "")) %>% 
+            as.numeric()
+      
+      Rating <- imdb %>% 
+            html_nodes(".ratingColumn.imdbRating") %>% 
+            html_nodes("strong") %>% 
+            html_text() %>% 
+            as.numeric()
+      
+      Votes <- imdb %>% 
+            html_nodes(".ratingColumn.imdbRating") %>% 
+            html_nodes("strong") %>% 
+            html_attrs() %>% 
+            map("title") %>% 
+            unlist()
+      
+      Cast <- imdb %>% 
+            html_nodes(".titleColumn") %>% 
+            html_nodes("a, #text") %>% 
+            html_attrs() %>% 
+            map("title") %>% 
+            unlist()
+      
+      Link <- imdb %>% 
+            html_nodes(".titleColumn") %>% 
+            html_nodes("a, #text") %>% 
+            html_attrs() %>% 
+            map("href") %>% 
+            unlist()
+      
+      imdb_df <- data.frame(Title, yearRelease, Rating,
+                            Cast, Votes, Link) %>% 
+            mutate(Link = paste0(imdb_home, Link))
+      
+      return(imdb_df)
+      
+}
+
+imdb_df <- extractFields(imdb)
+
+# Clean data -----------------------------------------------------------------------------
+
+imdb_df2 <- imdb_df %>% 
+      mutate(no.Votes = word(Votes, 4),
+             no.Votes = as.numeric(gsub(",", "", no.Votes)),
+             movieURL = paste0("["," Movie URL](",Link,")"),
+             Director = word(Cast, 1, sep = fixed(' (dir.)')),
+             Cast1 = word(Cast, 2, sep = fixed(', ')),
+             Cast2 = word(Cast, 3, sep = fixed(', ')))
+
+imdb_kable1 <- imdb_df2 %>% 
+      select(Title, yearRelease, Rating,
+             no.Votes, Director, Cast1,
+             Cast2, movieURL) %>% 
+      kable("html", align = "c", padding = 1,
+            caption = "imdb Top 250 Rated movies") %>% 
+      kable_styling("striped") %>% 
+      add_footnote(c("no.Votes = Number of imdb user ratings",
+                     "movieURL = Hyperlink to imdb movie homepage")) %>% 
+      scroll_box(height = "700px") 
+
+
+# Analyze and plot -----------------------------------------------------------------------
+
+
+plot1 <- ggplot(imdb_df2, aes(yearRelease)) +
+      geom_histogram(fill = "thistle", color = "thistle4",
+                     alpha = 0.8, binwidth = 2.5) +
+      geom_freqpoly(colour = "gray50", binwidth = 3) +
+      labs(title = "IMDb Top 250 Movies",
+           subtitle = "Year release") +
+      theme_minimal()
+ggsave("media/yearRelease_hist.png", plot1)
+
+
+plot2 <- ggplot(imdb_df2, aes(x = yearRelease, y = Rating)) +
+      geom_point() + 
+      geom_smooth(se = FALSE) + 
+      labs(title = "IMDb rating vs. Year of release") + 
+      theme_minimal()
+ggsave("media/yearRelease_rating.png", plot2)
+
+plot3 <- ggplot(imdb_df2, aes(x = no.Votes, y = Rating)) +
+      geom_point() +
+      geom_smooth(se = FALSE) +
+      labs(title = "IMDb rating vs. Number of votes") +
+      theme_minimal()
+ggsave("media/rating_noVotes.png", plot3)
+
+
---
+++ hw_10/rScripts/02_scrapeData.r
@@ -0,0 +1,17 @@
+## This scrip is used to clean the summary dataframe and plot data
+
+suppressPackageStartupMessages(library(tidyverse)) 
+suppressPackageStartupMessages(library(knitr))
+suppressPackageStartupMessages(library(kableExtra))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(rvest))
+
+
+# CLEAN DATA - Make 2nd data frame  ----------------------------------------------------------
+
+
+
+
+# ANALYSIS AND PLOT ----------------------------------------------------------------------
+
+
